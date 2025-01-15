1aaac462d83fdab5e424cf2d1fad5616bb19ad40
rakdav <rakdav@inbox.ru>
2025-01-15 18:56:28 +0200

    last

--- Titanic/Titanic.cs
+++ Titanic/Titanic.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Titanic;
 
 public partial class Titanic
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Passengerid { get; set; }
 
     public int Survived { get; set; }
